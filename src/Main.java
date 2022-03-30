0b80b922975f87da851be8b39d67a884e12de784
Olaf Seisler <olaf.seisler@gmail.com>
2022-03-30 12:16:44 +0300

    Lisasin meetodi mis küsib kasutajalt soovitud tegevust ning sooritab kõige sarnasema antud sõnele

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -66,11 +67,47 @@ public class Main {
 	static String kogusedEsita = "TOMAT: %d kg	REDIS: %d kg	PORGAND: %d kg";
 	
 	private static HashMap<String, Double> kogused = new HashMap<String, Double>(); // Sõnastik taime nimede ja kogustega
+	private static String[] tegevused = new String[] {"külva", "kasta", "väeta", "rohi", "lõika"};
 	
+	/**
+	 * Kontrollib kasutajalt saadud sisendit ning sooritab antud sõnale sarnaseima tegevuse
+	 */
 	public static void sisendiKontroll() {
-		// TODO: Kontrollida millise tegevusega sarnaneb 
-		// kasutaja sisestatud sõne kõige enam (vähemalt 3 tähte kattuvad)
-		// ning teha
+		// Loo scanner
+		Scanner sc = new Scanner(System.in);
+		boolean protsess = true;
+		String sarnaseimTegevus = "";
+		
+		// Küsi kuni saad sobiva vastuse
+		while (protsess) {
+			String[] vastus = sc.next().toLowerCase().split(" ");
+			
+			tagasi: // Label kust loopidest välja saab
+			for (String s : tegevused) {
+				int sarnasusi = 0;
+				for (int j = 0; j < s.length(); j++) {
+					// Kui sõna on piisavalt sarnane antud tegevusega, murra vali see ja murra välja
+					if (sarnasusi >= 3) {
+						sarnaseimTegevus = s;
+						protsess = false;
+						break tagasi; // Murra kahest loopist välja
+					}
+					
+					// Kui kasutaja antud sõnas on sarnane täht, lisa sarnasuspunkte
+					if (vastus[0].contains(s.substring(j, j + 1))) {
+						sarnasusi++; 
+					}
+				}
+			}
+			
+			// Kui sõnal pole piisavalt sarnasusi, küsi uuesti
+			if (sarnaseimTegevus.equals("")) {
+				System.out.println("Ei mõista soovitud tegevust. Proovi uuesti ");
+			} 
+		}
+		
+		System.out.println("Sooritan tegevuse : " + sarnaseimTegevus);
+		
 	}
 	
 	/**
@@ -98,6 +135,7 @@ public class Main {
 		
 		p.istuta(tomat, 0);
 		p.kasvata();
+		sisendiKontroll();
 		System.out.println(p.toString());
 		
 		// Graafiline osa
